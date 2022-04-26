d96e92c75850d9de4b1f351c84ba0e2caa69051e
Soham041201 <sattigeri.soham@gmail.com>
2022-04-26 09:09:44 +0530

    MUI added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,88 @@
-import logo from './logo.svg';
+import { Box, Button, Typography } from "@mui/material";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Box
+        sx={{
+          alignItems: "center",
+          display: "flex",
+          flexDirection: "column",
+          my: "15%",
+        }}
+      >
+        <Typography
+          sx={{
+            fontFamily: "Raleway",
+            fontWeight: "bold",
+            color: "white",
+            fontSize: ["2rem", "3rem", "4rem"],
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Sab Sunno.
+        </Typography>
+        <Typography
+          sx={{
+            fontFamily: "Raleway",
+            fontWeight: "bold",
+            color: "white",
+            fontSize: "1rem",
+          }}
+          display="inline"
+        >
+          #Connecting
+          <Typography
+            display="inline"
+            sx={{
+              color: "#07F0FF",
+              fontWeight: "bold",
+            }}
+          >
+            People
+          </Typography>
+          Through
+          <Typography
+            display="inline"
+            sx={{
+              color: "#07F0FF",
+              fontWeight: "bold",
+            }}
+          >
+            Thoughts
+          </Typography>
+        </Typography>
+
+        <Button
+          variant="contained"
+          disableElevation
+          sx={{
+            backgroundColor: "white",
+            my: 4,
+            width: 200,
+            borderRadius: "60px",
+            "&:hover": {
+              backgroundColor: "rgba(248, 248, 248, 0.8)",
+            },
+          }}
+        >
+          <Typography
+            sx={{
+              color: "black",
+              textTransform: "none",
+              fontFamily: "Raleway",
+              fontWeight: "bold",
+            }}
+          >
+            Get Started
+          </Typography>
+          <NavigateNextIcon
+            sx={{
+              color: "black",
+            }}
+          />
+        </Button>
+      </Box>
     </div>
   );
 }
