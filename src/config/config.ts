7a765759964ddb7cb2eeba220ecb057e9e727b5d
SOHAM SATTIGERI <sohamsattigeri@SOHAMs-MacBook-Pro.local>
2023-01-18 22:41:34 +0530

    Deploying backend

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
 
 const isLocal: boolean = false
-export const uri: String= isLocal ? 'http://localhost:8000': 'https://sab-sunno-service.onrender.com/'
+export const uri: String= isLocal ? 'http://localhost:8000': 'https://sab-sunno-service.onrender.com'
